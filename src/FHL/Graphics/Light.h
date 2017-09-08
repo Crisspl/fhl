7c031445c6b70bd3fec3bb26a0f32a89fded7aee
Criss <crisserpl2@gmail.com>
2017-09-08 22:48:50 +0200

    Modified a little Light creation methods

--- src/FHL/Graphics/Light.h
+++ src/FHL/Graphics/Light.h
@@ -18,9 +18,9 @@ namespace fhl
 		  bool operator==(const Light & _other) const;
 		  bool operator!=(const Light & _other) const;
 
-		  static constexpr Light infinite(Vec3f _dir, Color _color = Color::White(), float _illu = 1.f);
-		  static constexpr Light point(Vec3f _pos, Color _color = Color::White(), float _illu = 1.f, float _linear = 0.045f, float _quadratic = 0.0075f);
-		  static constexpr Light spot(Vec3f _pos, Vec3f _dir = Vec3f::forward(), Color _color = Color::White(), float _cutOff = 30.f, float _illu = 1.f, float _linear = 0.045f, float _quadratic = 0.0075f);
+		  static constexpr Light infinite(const Vec3f & _dir, const Color & _color = Color::White(), float _illu = 1.f);
+		  static constexpr Light point(const Vec3f & _pos, const Color & _color = Color::White(), float _illu = 1.f, float _linear = 0.045f, float _quadratic = 0.0075f);
+		  static constexpr Light spot(const Vec3f & _pos, const Vec3f & _dir = Vec3f::forward(), const Color & _color = Color::White(), float _cutOff = 30.f, float _illu = 1.f, float _linear = 0.045f, float _quadratic = 0.0075f);
 
 		  Vec3f position, direction;
 		  Color color;
@@ -30,7 +30,7 @@ namespace fhl
 		  Type type;
 	 };
 
-	 constexpr Light Light::infinite(Vec3f _dir, Color _color, float _illu)
+	 constexpr Light Light::infinite(const Vec3f & _dir, const Color & _color, float _illu)
 	 {
 		 return
 		 {
@@ -45,7 +45,7 @@ namespace fhl
 		 };
 	 }
 
-	 constexpr Light Light::point(Vec3f _pos, Color _color, float _illu, float _linear, float _quadratic)
+	 constexpr Light Light::point(const Vec3f & _pos, const Color & _color, float _illu, float _linear, float _quadratic)
 	 {
 		 return
 		 {
@@ -60,7 +60,7 @@ namespace fhl
 		 };
 	 }
 
-	 constexpr Light Light::spot(Vec3f _pos, Vec3f _dir, Color _color, float _cutOff, float _illu, float _linear, float _quadratic)
+	 constexpr Light Light::spot(const Vec3f & _pos, const Vec3f & _dir, const Color & _color, float _cutOff, float _illu, float _linear, float _quadratic)
 	 {
 		 return
 		 {
